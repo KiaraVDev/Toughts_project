b15d2f12b7e028ed27ccfdb40d165cbee99f623d
Kiara Victoria <83596877+NegrescoDemon02@users.noreply.github.com>
2024-06-19 10:55:49 -0300

    att

---
+++ db/conn.js
@@ -0,0 +1,15 @@
+const { Sequelize } = require('sequelize')
+
+const sequelize = new Sequelize('toughts2', 'root', 'vertrigo', {
+    host: 'localhost',
+    dialect: 'mysql',
+})
+
+try{
+    sequelize.authenticate()
+    console.log('Conectamos com sucesso! ✅')
+}catch(error){
+    console.log(`Não foi conectado ❌: ${error}`)
+}
+
+module.exports = sequelize
