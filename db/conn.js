52520dca1e6cd5c76aeed32c3c9c5da3dcd1d504
Kiara Victoria <83596877+NegrescoDemon02@users.noreply.github.com>
2024-06-21 08:37:16 -0300

    erro 1049 resolvido
    erro dado por não criar uma tabela

--- db/conn.js
+++ db/conn.js
@@ -1,32 +1,16 @@
-// const { Sequelize } = require('sequelize')
-
-// const sequelize = new Sequelize('buceta', 'root', 'vertrigo', {
-//     host: 'localhost',
-//     dialect: 'mysql',
-// });
-
-// try{
-//     sequelize.authenticate()
-//     console.log('Conectamos com sucesso! ✅')
-// }catch(err){
-//     console.log(`Não foi conectado ❌: ${err}`)
-// }
-
-// module.exports = sequelize
-
 const { Sequelize } = require('sequelize')
 
-const sequelize = new Sequelize('ryuzaki', 'root', 'vertrigo', {
+const sequelize = new Sequelize('toughts', 'root', 'vertrigo', {
     host: 'localhost',
     dialect: 'mysql',
-})
+});
 
 try{
-
     sequelize.authenticate()
-    console.log('Conectamos ao MySQL!')
-}catch(error){
-    console.log(`Não foi possivel se conectar: ${error}`)
+    console.log('Conectamos com sucesso! ✅')
+}catch(err){
+    console.log(`Não foi conectado ❌: ${err}`)
 }
 
-module.exports =  sequelize
+module.exports = sequelize
+
