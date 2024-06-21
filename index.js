1f6db5c2fa282893b56e09c5d7fe47f4100b8d2a
Kiara Victoria <83596877+NegrescoDemon02@users.noreply.github.com>
2024-06-21 10:38:34 -0300

    att

--- index.js
+++ index.js
@@ -30,9 +30,33 @@ app.use(
       logFn: function() {},
       path: require('path').join(require('os').tmpdir(), 'sessions'),
     }),
+
+    // Esse cookie faz com que expire o login em 24h
+
+    cookie: {
+      secure: false,
+      maxAge: 360000,
+      expires: new Date(Date.now() + 360000),
+      httpOnly: true
+    }
   }),
 )
 
+// Flash messages
+app.use(flash())
+
+// Public path
+app.use(express.static('public'))
+
+// Set session to res
+
+app.use((req, res, next) => {
+  if(req.session.userid){
+    res.locals.session = req.session
+  }
+
+  next()
+})
 
 conn
   .sync()
